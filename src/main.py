f62eae17787cfdfac1ed030b2dec19db4c8d0417
camille <camille.jandot@gmail.com>
2017-03-21 15:29:26 +0100

    load ecg data

---
+++ src/main.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 21 15:00:09 2017
+
+@author: camillejandot
+"""
+
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Load ECG data
+df = pd.read_table('../data/ecg/foetal_ecg.dat', sep="\s+", header=None)
+
+# Display the 8 channels
+channels = []
+for i in range(8):
+    channel = df[i+1]
+    channels.append(channel)
+    plt.figure()
+    plt.plot(channel[:500])
+    plt.title('Channel ' + str(i+1))
