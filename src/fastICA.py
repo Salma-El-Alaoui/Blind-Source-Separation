65da6525524153d63050884f6deaeb231ae84069
jachdou <juliette.achdou@free.fr>
2017-03-27 13:02:58 +0200

    continued fastISA

--- src/fastICA.py
+++ src/fastICA.py
@@ -15,7 +15,7 @@ def center(X):
     centered =  X - np.mean(X, axis =0)
     return np.transpose(centered)
 
-def whiten(X):
+def whiten(X, red_dim = None):
     Y = np.transpose(X)
     N, p = Y.shape
     Y= Y/np.sqrt(N-1)
@@ -67,20 +67,36 @@ def fastICA(X,n_iter=10):
     A = np.linalg.inv(W)
     return W,S,A
 
+def orthogonalize(W):
+    M = np.dot(W,W.T)
+    ret = np.real(np.linalg.inv(scipy.linalg.sqrtm(M))).dot(W)
+    return ret
+    
+
 def fastISA(X, dim, red_dim, T, sub_dim, maxiter, seed):
     
     Xtilda, R, R_inv = whiten(X)
     
     block_matrix=np.zeros(dim)
-    for i in range(dim%sub_dim):
+    for i in range(dim//sub_dim):
+        begin_block=(i-1)*sub_dim
         for j in range(sub_dim):
             for k in range(sub_dim):
-                offset=(i-1)*sub_dim
-                block_matrix[offset+j,offset+k]=1
+                block_matrix[begin_block+j,begin_block+k]=1
                 
-    W = rand(dim,red_dim) 
-    E= zeros(dim,red_dim)
-    E(1,rdim)=.0001;
-    W = real((W*W')^(-0.5))*W; % orthoganlize
+    W = np.random.rand(dim,red_dim) 
+    W = orthogonalize(W)
+    
+    for i in range(maxiter):
+        s = np.dot(W,X) 
+        s_square = s**2
+        block_subspace = block_matrix.dot(s_square)
+        
+        gamma = 0.1
+        g =  (gamma + block_subspace)**(-1/2.)
+        g_prime = -1/2.*(gamma + block_subspace)**(-3/2.)
+        
+        W = 
+
 
     
